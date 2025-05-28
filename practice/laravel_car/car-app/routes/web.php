b574f123386b5950e92617aa74b99a9efb7c37d7
Olga Kutuzova <Olga.Kutuzova@edu-its.it>
2025-05-28 08:59:03 +0200

    Comtinued some practice with Laravel

--- practice/laravel_car/car-app/routes/web.php
+++ practice/laravel_car/car-app/routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CarController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
 
 Route::view('/about', 'about')->name('about');
-
-Route::get('/car', [CarController::class, 'index']);
 
 
